3c7d6b1f67ab2d047e3f68a313462e0310149f6b
Karnuthala Sneha <karnuthala.sneha@ibm.com>
2023-02-17 18:32:20 +0530

    second commit

---
+++ src/main/java/com/application/project/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.application.project;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class HelloWorld {
+	@RequestMapping("/")
+	public String sayHello() {
+return "I am running fine";
+}
+}
